b47a44c815280d8a1034036ebc69e8d16fbadcb2
geebski <mlabenski123@gmail.com>
2023-06-07 22:32:14 +0000

    font edits

--- components/EditableField/EditableField.vue
+++ components/EditableField/EditableField.vue
@@ -4,8 +4,8 @@
       <span v-else @click="handleClick">{{ localValue }}</span>
     </td>
     <td v-else-if="templateNum == 1" class="border border-gray-700 px-2 md:px-4 py-1 md:py-2">
-      <input v-if="isEditing" v-model="localValue" class="font-bold text-lg md:text-xl mb-2 text-white" @blur="handleBlur">
-      <span v-else @click="handleClick">{{ localValue }}</span>
+      <input v-if="isEditing" v-model="localValue" class="bg-gray-700 text-white rounded px-2 py-1" @blur="handleBlur">
+      <span v-else @click="handleClick" class="font-bold text-lg md:text-xl mb-2 text-white">{{ localValue }}</span>
     </td>
   </template>
   
