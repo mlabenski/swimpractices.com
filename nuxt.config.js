3516cf464c2a0539cdad81e45220f4d56b19f5b4
geebski <mlabenski123@gmail.com>
2023-06-07 11:32:46 +0000

    generate fallback

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+  generate: {
+    fallback: true
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
