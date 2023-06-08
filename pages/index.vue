77179ac54e06490c1cd6102a4defb958c701b805
geebski <mlabenski123@gmail.com>
2023-06-08 12:38:13 -0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <template>
-  <div>
     <div class="container mx-auto px-4 py-2">
       <h1 class="text-4xl font-bold mb-4 pt-2">Swim Practice</h1>
 
@@ -50,8 +49,6 @@
       </div>
     </div>
   </nav>
-
-  </div>
 </template>
 
 <script>
