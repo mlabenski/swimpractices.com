2d450d405fe35c6f63d2a3aaed64ba5b59951256
geebski <mlabenski123@gmail.com>
2023-06-08 12:58:01 -0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' } 
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
