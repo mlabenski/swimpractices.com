7bca5a882aee099d3e377821d97e4dd73d719fa4
geebski <mlabenski123@gmail.com>
2023-06-10 19:25:23 -0400

    header for index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
+  <header class="bg-cyan-800 text-white text-lg px-4 py-2 fixed w-full z-50 flex items-center">
+      <h1 class="font-semibold">Swimpractices.com</h1>
+   </header>
     <div class="container mx-auto px-4 py-2">
-      <h1 class="text-4xl font-bold mb-4 pt-2">Swim Practice</h1>
-
       <div class="flex flex-col sm:flex-row justify-center">
         <GenerateSetModel v-if="isModalOpen" @close="closeModal" />
       </div>
@@ -53,7 +54,6 @@
   </nav>
   </div>
 </template>
-
 <script>
 import GenerateSetModel from '@/components/GenerateSetModel';
 import SetList from '@/components/SetList/SetList.vue';
