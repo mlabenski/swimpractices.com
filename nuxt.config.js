a93415d7ee33dda8d421a9ed0586037649b64581
geebski <mlabenski123@gmail.com>
2023-06-12 21:41:04 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/main.css'
+    '~/static/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
