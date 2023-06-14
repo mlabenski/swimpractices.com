0df9c5eb983df82bc27d0027e5b3a39d34dff176
geebski <mlabenski123@gmail.com>
2023-06-14 14:54:36 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
   <div>
     <header class="bg-gray-800 text-white text-lg px-4 py-2 fixed w-full z-50 flex items-center justify-between">
       <h1 class="font-semibold">Swimpractices.com</h1>
-      <div class="hidden md:block relative ml-auto">
-        <h1 class="font-semibold text-md">View the Daily Practice</h1>
-          <span class="material-icons text-white text-2xl">
-            lightbulb_outline
-          </span>
+      <div class="hidden md:flex relative ml-auto items-center">
+        <h1 class="font-semibold text-md mr-2">View the Daily Practice</h1>
+        <span class="material-icons text-white text-2xl mr-2">
+          lightbulb_outline
+        </span>
         <span class="absolute top-0 right-0 w-3 h-3 bg-red-500 rounded-full"></span>
       </div>
       <div class="md:hidden relative ml-auto">
