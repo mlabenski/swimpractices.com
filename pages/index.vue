6d8f527ec6ce412272b24d06928f146efb8f0bc7
geebski <mlabenski123@gmail.com>
2023-06-14 15:41:32 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <header class="bg-gray-800 text-white text-lg px-4 py-2 fixed w-full z-50 flex items-center justify-between">
       <h1 class="font-semibold">Swimpractices.com</h1>
       <div class="hidden md:flex relative ml-auto items-center">
-        <router-link :to="{ name: '3PMtTR93QWGvy2n1tlBC', params: { id: '3PMtTR93QWGvy2n1tlBC' } }" class="text-blue-600 underline">
+        <router-link :to="{ name: 'id', params: { id: '3PMtTR93QWGvy2n1tlBC' } }" class="text-blue-600 underline flex items-center">
           <h1 class="font-semibold text-md mr-2">View the Daily Practice</h1>
           <span class="material-icons text-white text-2xl mr-2">
             lightbulb_outline
@@ -11,6 +11,7 @@
           <span class="absolute top-0 right-0 w-3 h-3 bg-red-500 rounded-full"></span>
         </router-link>
       </div>
+
 
       <div class="md:hidden relative ml-auto">
         <button @click="toggleNotifications" class="focus:outline-none">
