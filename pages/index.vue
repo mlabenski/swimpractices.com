85b8d446f6c30045f528f31a96c1007d00a24845
geebski <mlabenski123@gmail.com>
2023-06-14 14:34:32 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <header class="bg-gray-800 text-white text-lg px-4 py-2 fixed w-full z-50 flex items-center justify-between">
       <h1 class="font-semibold">Swimpractices.com</h1>
       <div class="md:hidden relative ml-auto">
+      <h1 class="font-semibold sm:hidden">View the Daily Practice</h1>
         <button @click="toggleNotifications" class="focus:outline-none">
     <span class="material-icons text-white text-2xl">
       lightbulb_outline
