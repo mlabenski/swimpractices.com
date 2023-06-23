e6ac6577630607f0c3e5649ff8d639061f245542
geebski <mlabenski123@gmail.com>
2023-06-23 14:52:09 -0400

    Update index.vue

--- components/SeasonList/index.vue
+++ components/SeasonList/index.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="fixed z-10 inset-0 overflow-y-auto" v-if="seasons">
-    <div class="flex items-end justify-center min-h-screen">
+    <div class="flex items-center justify-center min-h-screen">
       <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
       <div class="bg-white rounded-lg px-4 pt-5 pb-4 overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full sm:p-6">
         <h3 class="text-lg leading-6 font-medium text-gray-900 mb-4">Add Practice to Season</h3>
-        <div v-for="season in seasons" :key="season.id" @click="expandSeason(season.id)" class="mb-2">
-          <button class="w-full text-black py-2 rounded-full bg-yellow-500 hover:bg-yellow-600 transition-colors">
+        <div v-for="season in seasons" :key="season.id" @click="expandSeason(season.id)" class="mb-2 flex justify-center">
+          <button class="w-3/5 text-black py-2 rounded-full bg-yellow-500 hover:bg-yellow-600 transition-colors">
             {{ season.title }}
           </button>
         </div>
-        <button class="w-full text-black py-2 rounded-full bg-yellow-500 hover:bg-yellow-600 transition-colors mb-4" @click="newSeasonForm = true">
-          Create New Season
-        </button>
+        <div class="flex justify-center mb-4">
+          <button class="w-3/5 text-black py-2 rounded-full bg-yellow-500 hover:bg-yellow-600 transition-colors" @click="newSeasonForm = true">
+            Create New Season
+          </button>
+        </div>
         <form v-if="expandedSeason || newSeasonForm" @submit.prevent="expandedSeason ? updateSeason() : createSeason()">
           <input v-model="seasonTitle" type="text" placeholder="Season title" required class="mb-2">
           <input v-model="seasonDescription" type="text" placeholder="Season description" required class="mb-2">
-          <button type="submit" class="w-full text-black py-2 rounded-full bg-green-500 hover:bg-green-600 transition-colors">
-            {{ expandedSeason ? 'Update Season' : 'Create Season' }}
-          </button>
+          <div class="flex justify-center">
+            <button type="submit" class="w-3/5 text-black py-2 rounded-full bg-green-500 hover:bg-green-600 transition-colors">
+              {{ expandedSeason ? 'Update Season' : 'Create Season' }}
+            </button>
+          </div>
         </form>
       </div>
     </div>
   </div>
 </template>
+
 
 <script>
 import {mapGetters} from "vuex";
