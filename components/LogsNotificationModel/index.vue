1917a06d5fcb1447f2811cf2884979ce7729f08e
mlabenski <mlabenski123@gmail.com>
2023-07-03 15:05:41 -0700

    notifications

--- components/LogsNotificationModel/index.vue
+++ components/LogsNotificationModel/index.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="notifications">
-    <div v-for="notification in notifications" :key="notification.id" class="notification">
-      {{ notification.message }}
-      <button @click="removeNotification(notification)">Dismiss</button>
+  <transition-group name="list" tag="div" class="fixed bottom-0 right-0 z-50">
+    <div v-for="notification in notifications" :key="notification.id" class="mb-4 mr-4 flex bg-white rounded-lg shadow-lg overflow-hidden animate-bounce">
+      <div class="flex-1 p-4">
+        <p class="font-semibold text-gray-800">{{ notification.message }}</p>
+      </div>
+      <button @click="removeNotification(notification)" class="text-white bg-red-500 hover:bg-red-600 rounded-l-none rounded-r px-4">
+        <span class="material-icons">
+          close
+        </span>
+      </button>
     </div>
-  </div>
+  </transition-group>
 </template>
 
 <script>
@@ -23,3 +29,13 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.list-enter-active, .list-leave-active {
+  transition: all .5s ease;
+}
+.list-enter, .list-leave-to {
+  transform: translateY(100px);
+  opacity: 0;
+}
+</style>
