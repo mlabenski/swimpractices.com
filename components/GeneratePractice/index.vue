77500abcb431ef79d920842c7b533afceaf108eb
mlabenski <mlabenski123@gmail.com>
2023-07-12 17:23:54 -0700

    practice generate improvement

--- components/GeneratePractice/index.vue
+++ components/GeneratePractice/index.vue
@@ -1,39 +1,34 @@
 <template>
-  <b-modal v-model="value" title="Generate Swim Practice" centered ok-only ok-variant="primary">
-    <div class="p-4 bg-white rounded shadow-xl">
-      <div class="grid grid-cols-2 gap-4 mb-4">
-        <div>
-          <label class="block text-gray-700" for="distance">Distance:</label>
-          <input v-model.number="practice.distance" id="distance" type="number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-        </div>
+  <b-modal v-model="value" title="Generate Swim Practice" centered size="md" class="shadow-lg">
+    <div class="px-5 py-3 bg-white">
+      <div class="mb-4 grid grid-cols-2 gap-4">
+        <b-form-group label="Distance:" label-for="distance">
+          <b-form-input v-model.number="practice.distance" id="distance" type="number" size="sm"></b-form-input>
+        </b-form-group>
 
-        <div>
-          <label class="block text-gray-700" for="poolSize">Pool Size:</label>
-          <b-form-select v-model="practice.poolSize" id="poolSize" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-            <option value="25 meters">25 meters</option>
-            <option value="50 meters">50 meters</option>
-            <option value="25 yards">25 yards</option>
+        <b-form-group label="Pool Size:" label-for="poolSize">
+          <b-form-select v-model="practice.poolSize" id="poolSize" size="sm">
+            <b-form-select-option value="25 meters">25 meters</b-form-select-option>
+            <b-form-select-option value="50 meters">50 meters</b-form-select-option>
+            <b-form-select-option value="25 yards">25 yards</b-form-select-option>
           </b-form-select>
-        </div>
+        </b-form-group>
       </div>
 
-      <div class="mb-4">
-        <label class="block text-gray-700" for="duration">Duration:</label>
-        <input v-model="practice.duration" id="duration" type="time" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-      </div>
+      <b-form-group label="Duration:" label-for="duration">
+        <b-form-input v-model="practice.duration" id="duration" type="time" size="sm"></b-form-input>
+      </b-form-group>
 
-      <div class="mb-4">
-        <label class="block text-gray-700">Acceptable Strokes:</label>
+      <b-form-group label="Acceptable Strokes:" label-for="strokes">
         <b-form-checkbox-group v-model="practice.strokes" id="strokes">
           <b-form-checkbox value="fly">Fly</b-form-checkbox>
           <b-form-checkbox value="back">Back</b-form-checkbox>
           <b-form-checkbox value="breast">Breast</b-form-checkbox>
           <b-form-checkbox value="freestyle">Freestyle</b-form-checkbox>
         </b-form-checkbox-group>
-      </div>
+      </b-form-group>
 
-      <div class="mb-4">
-        <label class="block text-gray-700">Acceptable Equipment:</label>
+      <b-form-group label="Acceptable Equipment:" label-for="equipment">
         <b-form-checkbox-group v-model="practice.equipment" id="equipment">
           <b-form-checkbox value="fins">Fins</b-form-checkbox>
           <b-form-checkbox value="snorkel">Snorkel</b-form-checkbox>
@@ -41,15 +36,15 @@
           <b-form-checkbox value="pull boy">Pull Boy</b-form-checkbox>
           <b-form-checkbox value="parachute">Parachute</b-form-checkbox>
         </b-form-checkbox-group>
-      </div>
+      </b-form-group>
 
-      <div class="text-right">
-        <b-button variant="success" class="text-black hover:text-black focus:text-black" @click="submitPractice">Submit</b-button>
-
+      <div class="text-right mt-4">
+        <b-button variant="primary" @click="submitPractice">Submit</b-button>
       </div>
     </div>
   </b-modal>
 </template>
+
 
 
 <script>
@@ -74,7 +69,7 @@ export default {
     async submitPractice() {
       // Generate the practice request sentence
       let sentence = `Generate a swim practice for the pool type ${this.practice.poolSize}, the practice should be ${this.practice.distance} long, and focus on the following strokes ${this.practice.strokes.join(', ')} and allow the following equipment ${this.practice.equipment.join(', ')}. The title should have a creative name, and the userID should be ${this.user}. The ID should be a number between 100 and 12000.`;
-
+      let responseString = null;
       try {
         // Send a POST request to the API
         let response = await axios.post('https://genhppurl.mlabenski.repl.co/generate/practice', {
@@ -82,6 +77,7 @@ export default {
         });
 
         // Handle the API response here, e.g., display a success message
+        responseString = response.data;
         console.log(response.data);
 
       } catch (error) {
@@ -92,6 +88,7 @@ export default {
       // Close the modal
       this.showModal = false;
       this.generatePracticeModal = false;
+      await this.$store.dispatch('notifications/addNotification', {message: `New practice created ${responseString}`, type: 2});
       this.$emit('input', false);
     },
   },
--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
