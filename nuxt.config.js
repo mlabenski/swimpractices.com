111dd4a2caaa6cc0b0cd3b0412250df714046651
mlabenski <mlabenski123@gmail.com>
2023-07-20 17:43:39 -0700

    might break the site

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
+  ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
