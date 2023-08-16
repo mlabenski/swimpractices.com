73e7b4d6fbbe84a0c5bd0102755c96ab0bbaf0ca
mlabenski <mlabenski123@gmail.com>
2023-08-15 20:44:06 -0700

    swipe animation testing

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/netlify-identity-widget.js', mode: 'client' },
-    '~/plugins/touch-events.js'
+    { src: '~/plugins/touch-events.js', mode: 'client'}
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
