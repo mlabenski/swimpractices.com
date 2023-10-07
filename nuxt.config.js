ec9670527df684d57f5152c2c60e62b42cadf5b3
mlabenski <mlabenski123@gmail.com>
2023-10-07 11:30:23 -0700

    '@nuxtjs/firebase'

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/firebase'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
