f41d17ec53d64041af7746b5c6c1a241232bbddb
mlabenski <mlabenski123@gmail.com>
2023-11-27 20:02:22 -0800

    i meant to do server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: true,
-  target: 'universal',
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
