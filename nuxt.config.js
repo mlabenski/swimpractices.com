b6bfaa11aa7d9b061ae69a43da3c1d2badd82341
mlabenski <mlabenski123@gmail.com>
2023-11-27 20:40:59 -0800

    remove server target

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: true,
-  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
