cff7e4b7aca3d9bc26adcbb22a8e94fff310b29d
mlabenski <mlabenski123@gmail.com>
2024-05-01 23:51:08 -0400

    invoke firestore /users on signup

--- store/auth/index.js
+++ store/auth/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const ACTION_KEY_LOGIN = 'login'
 export const ACTION_KEY_SIGNUP = 'signup'
 
@@ -57,7 +59,7 @@ export const actions = {
   },
   open({ commit }, action) {
     this.$netlifyIdentity.open(action)
-    this.$netlifyIdentity.on(action, (user) => {
+    this.$netlifyIdentity.on(action, async (user) => {
       commit('SET_USER', {
         username: user.user_metadata.full_name,
         email: user.email,
@@ -66,6 +68,19 @@ export const actions = {
         uuid: user.user_metadata.uuid
       })
       this.$netlifyIdentity.close()
+
+      // Call the Google Cloud Function after signup
+      if (action === ACTION_KEY_SIGNUP) {
+        try {
+          await axios.post(' https://us-central1-swimpractices-92836.cloudfunctions.n', {
+            username: user.user_metadata.full_name,
+            email: user.email,
+            uuid: user.id // Assuming 'id' is used as the uuid
+          });
+        } catch (error) {
+          console.error('Error calling cloud function:', error);
+        }
+      }
     })
   }
 }
