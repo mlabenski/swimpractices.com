ec525d9d34bb91d7b656a4d9ec1f89488560a0eb
mlabenski <mlabenski123@gmail.com>
2026-03-07 20:02:09 -0500

    login button fix

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,13 +6,15 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex"
+import { mapState } from "vuex"
 import AuthModal from "@/components/AuthModal/index.vue"
 
 export default {
   components: { AuthModal },
   computed: {
-    ...mapGetters("auth", ["authModalOpen"]),
+    ...mapState({
+      authModalOpen: (state) => state.auth && state.auth.authModalOpen === true,
+    }),
   },
 }
 </script>
